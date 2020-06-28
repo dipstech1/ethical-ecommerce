534350daec7571a69611a29cc5aab6d3c9ebd224
dipstech1 <panja.souradeep@gmail.com>
2020-06-28 22:35:39 +0530

    filter product

--- src/app/preauth/product-list/product-list/product-list.component.ts
+++ src/app/preauth/product-list/product-list/product-list.component.ts
@@ -12,6 +12,32 @@ export class ProductListComponent implements OnInit, OnDestroy {
  
 
   productList = [];
+  sortByFilter = [
+    {
+      sortId:1,
+      sortName:"Popularity"
+    },
+    {
+      sortId:2,
+      sortName:"Low - Hight Price"
+    },
+    {
+      sortId:3,
+      sortName:"High - Low Price"
+    },
+    {
+      sortId:4,
+      sortName:"Average Rating"
+    },
+    {
+      sortId:5,
+      sortName:"A - Z Order"
+    },
+    {
+      sortId:6,
+      sortName:"Z - A Order"
+    }
+  ]
   protected ngUnsubscribe: Subject<void> = new Subject<void>();
 
   
@@ -22,8 +48,33 @@ export class ProductListComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((res:any) => {
         this.productList = [...res];
-        console.log("Product List ", this.productList);
       })
+  }
+
+  /**
+   * @param sortId
+   * Sort product list based on selected sort  
+  */
+  sortItem(sortId:number){
+    console.log("B ", this.productList)
+    if(sortId == 1){
+      this.productList = this.productList.sort((a,b) => parseFloat(b.ratings) - parseFloat(a.ratings));
+    }
+
+    if(sortId == 2){
+      this.productList = this.productList.sort((a,b) => parseFloat(a.price) - parseFloat(b.price));
+    }
+
+    if(sortId == 3){
+      this.productList = this.productList.sort((a,b) => parseFloat(b.price) - parseFloat(a.price));
+    }
+
+    if(sortId == 5){
+      this.productList = this.productList.sort((a,b) => a.productName.localeCompare(b.productName));
+    }
+
+
+
   }
 
   ngOnDestroy(): void {
