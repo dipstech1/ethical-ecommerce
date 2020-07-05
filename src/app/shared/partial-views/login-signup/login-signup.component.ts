55e2d7e177fe8b9682aa949199d6369ebeef5617
sinhashreya707 <sinhashreya707@gmail.com>
2020-07-05 22:00:25 +0530

    Added validation

--- src/app/shared/partial-views/login-signup/login-signup.component.ts
+++ src/app/shared/partial-views/login-signup/login-signup.component.ts
@@ -12,37 +12,45 @@ export class LoginSignupComponent implements OnInit {
   registerForm: FormGroup;
   loginForm: FormGroup;
   submitted = false;
+  loginSubmitted=false;
 
-  constructor(private formBuilder: FormBuilder,public activeModal: NgbActiveModal
-    ) { 
-      this.loginForm = this.formBuilder.group({     
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]]
-      });
-      this.registerForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        cpassword: ['', [Validators.required, Validators.minLength(6)]]
-    });
-    }
+  constructor(private formBuilder: FormBuilder, public activeModal: NgbActiveModal)
+    {}    
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group
+    ({     
+      emails: ['', [Validators.required, Validators.email]],
+      passwords: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      cpassword: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  }
+
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+  get loginFormControl() {
+    return this.loginForm.controls;
   }
   closeModal() {
     this.activeModal.close('Modal Closed');
   }
   onSubmit() {
     this.submitted = true;
-  console.log("here it is submitted");
     // stop here if form is invalid
     if (this.registerForm.invalid) {
-      alert(this.registerForm.invalid);
-        return;
+      return;
     }
-
 }
 onLogin(){
-  
+  this.loginSubmitted=true;
+  if (this.loginForm.invalid) {
+    return;
+  }
 }
 }
