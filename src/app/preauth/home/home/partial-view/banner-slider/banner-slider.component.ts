a9500c9a8fa3daa3a3a26d61c9ce46c2f4a8b4c6
dipstech1 <panja.souradeep@gmail.com>
2020-07-08 23:43:47 +0530

    addign carousel

--- src/app/preauth/home/home/partial-view/banner-slider/banner-slider.component.ts
+++ src/app/preauth/home/home/partial-view/banner-slider/banner-slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-banner-slider',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerSliderComponent implements OnInit {
 
-  constructor() { }
+  slider = [
+    {
+      imgSrc : "../../../../../../assets/img/home/hero-slider/05.jpg",
+      t1: "Check our all",
+      t2: "Smartphones",
+      t3:" Accessories collection"
+    },
+    {
+      imgSrc : "../../../../../../assets/img/home/hero-slider/04.jpg",
+      t1: "View the",
+      t2: "Gadgets",
+      t3:" Gadgets collection"
+    },
+    {
+      imgSrc : "../../../../../../assets/img/home/hero-slider/06.jpg",
+      t1: "Jump into",
+      t2: "World",
+      t3:" Accessories collection"
+    }
+  ]
+
+  constructor(config: NgbCarouselConfig) {  
+    
+  }  
 
   ngOnInit() {
   }
--- src/app/preauth/home/home/partial-view/promo-banner/promo-banner.component.ts
+++ src/app/preauth/home/home/partial-view/promo-banner/promo-banner.component.ts
@@ -10,7 +10,7 @@ export class PromoBannerComponent implements OnInit, OnChanges {
   // tslint:disable-next-line: no-input-rename
   @Input('promoBanners') promoBanners;
   constructor(config: NgbCarouselConfig) {  
-    config.interval = 2000;  
+    config.interval = 200000;  
     config.wrap = true;  
     config.keyboard = false;  
     config.pauseOnHover = false;  
